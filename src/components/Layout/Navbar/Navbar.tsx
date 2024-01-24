6258e60a12cc402801521e956ab50c50045db348
Tommy Grabowski <tommy.grabowski@velocitor.com>
2024-01-24 15:01:45 -0500

    Added Logo to navbar

--- src/components/Layout/Navbar/Navbar.tsx
+++ src/components/Layout/Navbar/Navbar.tsx
@@ -1,3 +1,4 @@
+import { Avatar, AvatarFallback, AvatarImage } from '@/components/ui/avatar';
 import NavbarLinks from './NavbarLinks/NavbarLinks';
 import NavbarUser from './NavbarUser/NavbarUser';
 
@@ -5,6 +6,13 @@ export default function Navbar() {
   return (
     <nav>
       <header>
+        <Avatar>
+          <AvatarImage
+            src="https://vtrackuat.velsol.com/Content/images/VTrack-Logo-Small.png"
+            alt="Velocitor Logo"
+          />
+          <AvatarFallback>VH</AvatarFallback>
+        </Avatar>
         <h1>Velocitor Helpdesk</h1>
         <NavbarLinks />
       </header>
